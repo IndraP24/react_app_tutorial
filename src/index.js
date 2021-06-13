fea6f1f688815f304212f474c3484c970d2403f1
IndraP24 <indrashis985@gmail.com>
2021-06-13 20:57:33 +0530

    Handled indentations

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ class Game extends React.Component {
     }
   }
   
-  // ========================================
+// ========================================
   
 ReactDOM.render(
     <Game />,
