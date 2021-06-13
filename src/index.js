9a8417bd0d0413de5ffb00c6f47b419e6a714dfd
IndraP24 <indrashis985@gmail.com>
2021-06-13 20:41:48 +0530

    Update react dom render

--- src/index.js
+++ src/index.js
@@ -6,18 +6,43 @@ import './index.css';
 /* This component renders a single button as a Square */
 class Square extends React.Component {
     render() {
-        return(
-            <button className="square">
-                {this.props.value}
-            </button>
-        );
+      return (
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
     }
-}
+  }
 
 /* This component renders all the 9 Squares in the game */
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+                </div>
+            </div>
+        );
     }
 }
 
