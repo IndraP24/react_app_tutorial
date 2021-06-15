100edfa369c221c45a2934a03356fad468c49232
IndraP24 <indrashis985@gmail.com>
2021-06-15 19:19:12 +0530

    Convert Square to function component

--- src/index.js
+++ src/index.js
@@ -4,18 +4,13 @@ import './index.css';
 
 
 /* This component renders a single button as a Square */
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 /* This component renders all the 9 Squares in the game */
 class Board extends React.Component {
